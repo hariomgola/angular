57a8888afb2ac9665d43e9f8e37fdc7f02c7c8f0
hariom.kumar <hariom987316@gmail.com>
2023-12-02 11:57:29 +0530

    Adding new angular project

---
+++ Notes/notes.js
@@ -0,0 +1,30 @@
+const angularData = {
+  "Introduction to Angular": `
+  # Introduction to Angular
+    - Angular is javaScript frameWork to allow you to craete reactive Single-page Application.
+    - Single page application doesn't refresh/load when navigate around the pages.
+  # Angular Concept
+    - Basic (binding and structure)
+    - Component and DataBindings
+    - Directives
+    - Services & Dependency Injection
+    - Routing
+    - Observables
+    - Forms
+    - Pipes
+    - Http
+    - Authentication
+    - Optimization & NgModules
+    - Deployment
+    - Animation & Testing
+  # Angular Different Version
+    - Angular (Angular 1) have issue with performance for big application and it also termed as angular
+    - Angular 2 - Released in 2016 and have many advantage over the orgional angular js
+    - Angular 4 - Angular 3 was skipped due to internal issue as well as naming convention
+    - Angular 9/10/11/12---17 - Angular team release every new version of angular in 6 months and follows symentaning approach
+    - In Simple Angular is the end is a JS framework, changing your DOM('HTML') at RunTime!
+
+  `,
+};
+
+module.exports = { angularData };
--- learn-ng/README.md
+++ learn-ng/README.md
@@ -1,6 +1,6 @@
-# SimpleConcept
+# LearnNg
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.8.
 
 ## Development server
 
