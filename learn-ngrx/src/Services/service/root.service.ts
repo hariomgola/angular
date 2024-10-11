a493343a98eaf0cb3b5e80f1fd42267a030e5261
hariom <hariom987316@gmail.com>
2024-10-11 11:01:18 +0530

    [Adding template for api call]

--- learn-ngrx/src/Services/service/root.service.ts
+++ learn-ngrx/src/Services/service/root.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RootService {
-  constructor() {}
-  getAllUsers() {}
+  private baseUrl = 'https://hari.com';
+  constructor(private _httpClient: HttpClient) {}
+
+  // Get User Data call
+  getApiCall(handler: string, payload?: any) {
+    return this._httpClient.get(`${this.baseUrl}/${handler} `, payload).pipe(
+      map((responseData) => {
+        return responseData;
+      }),
+      catchError((errorData) => {
+        return throwError(() => errorData);
+      })
+    );
+  }
 }
--- learn-ngrx/src/app/app.component.ts
+++ learn-ngrx/src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RootService } from '../Services/service/root.service';
+import { NgFor, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgTemplateOutlet],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'learn-ngrx';
+  userData: any;
+
   constructor(private _rootService: RootService) {}
+
   onApiCall() {
     // Direct Service call
-    this._rootService.getAllUsers();
+    this._rootService.getApiCall('users').subscribe((data) => {
+      this.userData = data;
+    });
   }
 }
